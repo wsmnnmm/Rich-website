ef8daceaadb8a846655faa79dfdd62822cc0573a
wsmnnmm <1832004940@qq.com>
2022-02-15 13:03:47 +0800

    deploy

---
+++ precache-manifest.833fa6297d2fbe423aa70abab4f74985.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d886ef5d6b2544698316",
+    "url": "/Rich-website/css/app.89e6116b.css"
+  },
+  {
+    "revision": "7989ad304df299f62c8fd97e370dbf1c",
+    "url": "/Rich-website/index.html"
+  },
+  {
+    "revision": "d886ef5d6b2544698316",
+    "url": "/Rich-website/js/app.56373c7f.js"
+  },
+  {
+    "revision": "55c38cddeabbdb6f81e8",
+    "url": "/Rich-website/js/chunk-vendors.41c0778c.js"
+  },
+  {
+    "revision": "26ed1defac65085b9bc82a018d2aca2b",
+    "url": "/Rich-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Rich-website/robots.txt"
+  }
+]);
